86b1ca93e5ff4d83b21599b224263258fe0fc78d
AlexzandreChui <113033697+AlexzandreChui@users.noreply.github.com>
2022-09-26 09:14:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Asleep)
+})
